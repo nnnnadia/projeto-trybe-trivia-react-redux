6a505666d41c57bd99cfb3332694d4ba5ed5622d
Aron Adams <aronrapetto@hotmail.com>
2022-06-28 17:35:10 -0300

    fetch thunk criado por engano, esboço mantido para reaproveitamento

---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,29 @@
+// import { SUCCESS, FAIL } from '../actions/actionTypes';
+
+// const INITIAL_STATE = {
+//   name: '',
+//   assertions: 0,
+//   score: 0,
+//   gravatarEmail: '',
+//   token: '',
+//   error: '',
+// };
+
+// const playerReducer = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//   case SUCCESS:
+//     return {
+//       ...state,
+//       token: action.payload,
+//     };
+//   case FAIL:
+//     return {
+//       ...state,
+//       error: action.payload,
+//     };
+//   default:
+//     return state;
+//   }
+// };
+
+// export default playerReducer;
