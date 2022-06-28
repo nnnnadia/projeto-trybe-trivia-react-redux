b76520508c178e645a7fa51e0112881d706c98f3
Aron Adams <aronrapetto@hotmail.com>
2022-06-28 17:33:36 -0300

    page game criada

---
+++ src/pages/Game.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <h1>Game page</h1>
+    );
+  }
+}
+
+// Game.propTypes = {
+//   fetch: PropTypes.func.isRequired,
+// };
+
+// const mapDispatchToProps = (dispatch) => ({
+//   fetch: () => dispatch(fetchToken()),
+// });
+
+export default Game;
