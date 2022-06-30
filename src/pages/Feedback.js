52a5caf69914aa7e5b8c189e934f4aa2ed9430d2
Aron Adams <aronrapetto@hotmail.com>
2022-06-30 15:53:11 -0300

    renderização condicional da mensagem de feedback implementada

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,10 +6,20 @@ import Header from '../Components/Header';
 class Feedback extends React.Component {
   render() {
     const { assertions } = this.props;
+    const MIN_ASSERTIONS = 3;
     return (
       <main>
         <h1>Feedback Page</h1>
         <Header />
+        {
+          (assertions < MIN_ASSERTIONS)
+            ? (
+              <h2 data-testid="feedback-text">Could be better...</h2>
+            )
+            : (
+              <h2 data-testid="feedback-text">Well Done!</h2>
+            )
+        }
       </main>
     );
   }
