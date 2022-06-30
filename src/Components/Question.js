e564ffadba438a286fc7ab8b6d07aa5deaa86d81
Aron Adams <aronrapetto@hotmail.com>
2022-06-29 23:55:28 -0300

    requisito 6 concluido

---
+++ src/Components/Question.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Question extends React.Component {
+  state = {
+    answersList: [],
+    correctAnswerIndex: 0,
+  }
+
+  componentDidMount() {
+    const {
+      question: {
+        correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswers,
+      },
+    } = this.props;
+    const VARIAVEL_RANDOMICA = 0.5;
+
+    const answersList = [...incorrectAnswers]
+      .sort(() => Math.random() - VARIAVEL_RANDOMICA);
+
+    const randomIndex = Math.round(Math.random() * incorrectAnswers.length);
+    answersList.splice(randomIndex, 0, correctAnswer);
+
+    this.setState({
+      answersList,
+      correctAnswerIndex: randomIndex,
+    });
+  }
+
+  render() {
+    const {
+      question: {
+        category,
+        difficulty,
+        question,
+      },
+    } = this.props;
+    const { answersList, correctAnswerIndex } = this.state;
+    return (
+      <div>
+        <h2 data-testid="question-text">{ question }</h2>
+        <h4 data-testid="question-category">{ category }</h4>
+        <h4>{ difficulty }</h4>
+        <div data-testid="answer-options">
+          {
+            answersList.map((answer, index) => {
+              if (index === correctAnswerIndex) {
+                return (
+                  <button
+                    data-testid="correct-answer"
+                    type="button"
+                    key={ answer }
+                  >
+                    { answer }
+                  </button>
+                );
+              }
+              return (
+                <button
+                  data-testid={ `wrong-answer-${index}` }
+                  type="button"
+                  key={ answer }
+                >
+                  { answer }
+                </button>
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+Question.propTypes = {
+  question: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    correct_answer: PropTypes.string.isRequired,
+    difficulty: PropTypes.string.isRequired,
+    incorrect_answers: PropTypes.arrayOf(PropTypes.string).isRequired,
+    question: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Question;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
+import Question from '../Components/Question';
 import fetchTriviaQuestions from '../service/fetchTriviaQuestions';
 
-export default class Game extends Component {
+class Game extends Component {
   state = {
     questions: [],
-    index: 0,
-    correctIndex: 0,
+    renderIndex: 0,
     // answered: false,
   }
 
   async componentDidMount() {
-    const six = 6;
     const { history } = this.props;
     const { results } = await fetchTriviaQuestions();
     if (!results.length) {
@@ -22,77 +21,32 @@ export default class Game extends Component {
 
     this.setState({
       questions: [...results],
-      correctIndex: Math.floor(Math.random() * six),
     });
   }
 
   nextQuestion = () => {
-    const six = 6;
     this.setState((prevState) => ({
-      index: prevState.index + 1,
-      correctIndex: Math.floor(Math.random() * six),
+      renderIndex: prevState.renderIndex + 1,
     }));
   }
 
   render() {
-    const half = 0.5;
-    const { questions, index, correctIndex } = this.state;
+    const { questions, renderIndex } = this.state;
     return (
-      <section>
+      <main>
         <Header />
         <h1>Game page</h1>
+        {
+          questions.map((question, index) => (
+            <section key={ question.question }>
+              {
+                renderIndex === index && (<Question question={ question } />)
+              }
+            </section>
+          ))
+        }
         <button type="button" onClick={ this.nextQuestion }>Próximo</button>
-        { questions.map(({
-          category,
-          incorrect_answers: incorrects,
-          correct_answer: correct,
-          question,
-          difficulty,
-        }, index2) => {
-          if (index === index2) {
-            return (
-              <div key={ question }>
-                <h1 data-testid="question-text">{ question }</h1>
-                <h2 data-testid="question-category">{ category }</h2>
-                <h3>{ difficulty }</h3>
-                <div data-testid="answer-options">
-                  { incorrects.sort(() => Math.random() - half)
-                    .map((incorrect, index3) => {
-                      if (correctIndex === index3) {
-                        return (
-                          <div>
-                            <button
-                              type="button"
-                              data-testid="correct-answer"
-                            >
-                              { correct }
-                            </button>
-                            <button
-                              type="button"
-                              key={ incorrect }
-                              data-testid={ `wrong-answer-${index3}` }
-                            >
-                              { incorrect }
-                            </button>
-                          </div>
-                        );
-                      }
-                      return (
-                        <button
-                          type="button"
-                          key={ incorrect }
-                          data-testid={ `wrong-answer-${index3}` }
-                        >
-                          { incorrect }
-                        </button>
-                      );
-                    })}
-                </div>
-              </div>
-            );
-          } return null;
-        })}
-      </section>
+      </main>
     );
   }
 }
@@ -102,10 +56,5 @@ Game.propTypes = {
     push: PropTypes.func.isRequired,
   }).isRequired,
 };
-// Game.propTypes = {
-//   fetch: PropTypes.func.isRequired,
-// };
 
-// const mapDispatchToProps = (dispatch) => ({
-//   fetch: () => dispatch(fetchToken()),
-// });
+export default Game;
