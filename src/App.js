0123e8f9c58523e4771d8801a8a8919cc8001b24
Aron Adams <aronrapetto@hotmail.com>
2022-06-30 15:44:16 -0300

    add rota para feedback page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 // import logo from './trivia.png';
 import './App.css';
 
@@ -14,9 +15,10 @@ export default function App() {
         <p>SUA VEZ</p>
       </header> */}
       <Switch>
+        <Route path="/feedback" component={ Feedback } />
         <Route path="/game" component={ Game } />
-        <Route exact path="/" component={ Login } />
         <Route path="/settings" component={ Settings } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </div>
   );
