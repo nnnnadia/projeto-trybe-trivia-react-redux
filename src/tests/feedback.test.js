ef800c473d55fe6881416fa1880b6d746d7ee17e
HilltonDev <specialforcescommandosbra@gmail.com>
2022-06-30 19:21:47 +0000

    REQUISITO 17 - Implementacao dos testes

--- src/tests/feedback.test.js
+++ src/tests/feedback.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import { screen} from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Feedback from '../pages/Feedback';
 
 describe('Teste a página de Feedback', () => {
@@ -18,7 +19,7 @@ describe('Teste a página de Feedback', () => {
     expect(screen.getByTestId("header-score")).toBeInTheDocument();
   });
 
-  it('03 - Renderiza na tela mensagem para jogador', () => {
+  it('03 - Renderiza na tela mensagem de feedback para jogador', () => {
     renderWithRouterAndRedux(<Feedback />);
     expect(screen.getByTestId("feedback-text")).toBeInTheDocument();
   });
@@ -33,5 +34,27 @@ describe('Teste a página de Feedback', () => {
     renderWithRouterAndRedux(<Feedback />);
     expect(screen.getByTestId("btn-ranking")).toBeInTheDocument();
     expect(screen.getByTestId("btn-play-again")).toBeInTheDocument();
+  });
+
+  it('06 - Testa se ao clicar no botão \'Ranking\' a navegação acontece normalmente', async () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const rankingPage = screen.getByTestId("btn-ranking");
+    userEvent.click(rankingPage);
+    await waitFor(
+      () => expect(screen.queryAllByText(/Ranking/i)).toBeInTheDocument(),
+      {timeout: 3000}
+    );
+    expect(window.location.pathname).toBe('/ranking');
+  });
+
+  it('07 - Testa se ao clicar no botão \'Jogar Novamente\' a navegação acontece normalmente', async () => {
+    renderWithRouterAndRedux(<Feedback />);
+    const rankingPage = screen.getByTestId("btn-play-again");
+    userEvent.click(rankingPage);
+    await waitFor(
+      () => expect(screen.queryAllByText(/Play/i)).toBeInTheDocument(),
+      {timeout: 3000}
+    );
+    expect(window.location.pathname).toBe('/');
   });
 });
