4f68c9620ed812eef64b5cdacfa38b03507eb9f4
Aron Adams <aronrapetto@hotmail.com>
2022-06-30 15:44:01 -0300

    Feedback page criada, componente header renderizado

---
+++ src/pages/Feedback.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Feedback Page</h1>
+        <Header />
+      </main>
+    );
+  }
+}
+
+export default Feedback;
