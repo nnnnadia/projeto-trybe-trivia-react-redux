d1a2b422e20e9aba5c9e47f6d001a5afec8aeb56
Aron Adams <aronrapetto@hotmail.com>
2022-06-30 15:47:13 -0300

    Feedback page conectada com o estado global

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
     return (
       <main>
         <h1>Feedback Page</h1>
@@ -12,4 +15,12 @@ class Feedback extends React.Component {
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.playerReducer.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
